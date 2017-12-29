b810f6698ba09f0d3679107ba3d2287a061d20cc
Aniol Santacreu <aniolsantacreusalva@gmail.com>
2017-12-29 17:01:38 +0100

    Creates Window and Updates to SDL2

--- sample/AStarPathFindingSample.cpp
+++ sample/AStarPathFindingSample.cpp
@@ -5,11 +5,23 @@
 
 #pragma comment(linker, "/INCLUDE:_mainCRTStartup")
 
-//#include "SDL.h"
-
 
 AStarPathFindingSample::AStarPathFindingSample() {
+	screen = NULL;
+}
 
+
+
+bool AStarPathFindingSample::Init() {
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+		return false;
+	}
+
+	if ((screen = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL)) == NULL) {
+		return false;
+	}
+
+	return true;
 }
 
 
@@ -23,6 +35,12 @@ int AStarPathFindingSample::Run() {
 int main(int argc, char* argv[]) {
 	AStarPathFindingSample aStarPathFindingSample;
 
+	if (aStarPathFindingSample.Init()) {
+		while (true) {
+			aStarPathFindingSample.Run();
+		}
+	}
 
-	return aStarPathFindingSample.Run();
+
+	return 0;
 }
--- sample/AStarPathFindingSample.h
+++ sample/AStarPathFindingSample.h
@@ -2,9 +2,15 @@
 
 //#include "resource.h"
 
+#include "SDL.h"
+
 class AStarPathFindingSample {
 	public:
 		AStarPathFindingSample();
 
+		bool Init();
 		int Run();
+
+	private:
+		SDL_Window* screen;
 };
