41cf30a653b5004ea43f53e2c5219341c560fb1a
Aniol Santacreu <aniolsantacreusalva@gmail.com>
2017-12-29 16:23:24 +0100

    Adds sample project

---
+++ sample/AStarPathFindingSample.cpp
@@ -0,0 +1,28 @@
+
+#include "stdafx.h"
+
+#include "AStarPathFindingSample.h"
+
+#pragma comment(linker, "/INCLUDE:_mainCRTStartup")
+
+//#include "SDL.h"
+
+
+AStarPathFindingSample::AStarPathFindingSample() {
+
+}
+
+
+
+int AStarPathFindingSample::Run() {
+	return 0;
+}
+
+
+
+int main(int argc, char* argv[]) {
+	AStarPathFindingSample aStarPathFindingSample;
+
+
+	return aStarPathFindingSample.Run();
+}
